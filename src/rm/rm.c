06358a55d6266d768b96d0732ccfb287e05c75c7
Rafael Leira Osuna <rafael.leira@uam.es>
2017-02-28 12:59:51 +0100

    another param check to rm

--- src/rm/rm.c
+++ src/rm/rm.c
@@ -21,18 +21,22 @@ static void app_usage (void) {
 	    "rm");
 }
 
+size_t n_files;
+char const *const *file;
+
 static void app_paramCheck (void) {
 	int stopExecution = 0;
 
+	if (n_files == 0) {
+		stopExecution = 1;
+		printf ("PARAM-ERROR: Need to pass at least one file to remove\n");
+	}
 	if (stopExecution) {
 		printf ("\n");
 		app_usage ();
 		exit (-1);
 	}
 }
-
-size_t n_files;
-char const *const *file;
 
 void app_config (int argc, char **argv) {
 	int c;
@@ -42,7 +46,7 @@ void app_config (int argc, char **argv) {
 		int option_index = 0;
 
 		c = getopt_long (argc, argv, ":h", long_options, &option_index);
-		puts("exe");
+		puts ("exe");
 		/* Detect the end of the options. */
 		if (c == -1)
 			break;
@@ -50,7 +54,7 @@ void app_config (int argc, char **argv) {
 		switch (c) {
 			case 'h':
 				app_usage ();
-				//exit (1);
+				// exit (1);
 				break;
 			case '?':
 				puts ("?");
@@ -63,8 +67,8 @@ void app_config (int argc, char **argv) {
 		}
 	}
 
-    n_files = argc - optind;
-    file = (char const *const *) argv + optind;
+	n_files = argc - optind;
+	file    = (char const *const *)argv + optind;
 
 	app_paramCheck ();
 	return;
@@ -79,14 +83,13 @@ void app_init (nvmeRaid *raid) {
 void app_run (nvmeRaid *raid) {
 	size_t i;
 
-	for (i=0;i<n_files;i++){
-		printf("Trying to remove file \"%s\"...",file[i]);
-		if(delFile (raid, file[i])){
-			printf("OK\n");
-		}else{
-			printf("ERROR\n");
+	for (i = 0; i < n_files; i++) {
+		printf ("Trying to remove file \"%s\"...", file[i]);
+		if (delFile (raid, file[i])) {
+			printf ("OK\n");
+		} else {
+			printf ("ERROR\n");
 		}
-	
 	}
 	return;
 }
